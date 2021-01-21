25721d8fe86d200e4f71b8e36dbc7dabf4235414
Nicolas Lopez <unacorbatanegra@gmail.com>
2021-01-21 11:20:45 -0300

    simple complexity anotation

--- arrays.dart
+++ arrays.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 void main() {
-
+  reverseList();
 }
 
 void sumFromNumber(int number) {
@@ -25,13 +25,32 @@ void largestSmallNumber() {
   }
   print('largest $l, smaller $s');
 }
+/// Estimate completixity
+/// primera linea `1 + n + 1 +n  ` = `2n+2`
+/// segunda linea
 
 void duplicateNumberOfIntegerArray() {
-  final array = [1, 2, 3, 4, 5, 4];
+  final array = [1, 2, 3, 4, 3, 5, 4];
   for (var i = 0; i < array.length; i++) {
     for (var j = i + 1; j < array.length; j++) {
       if (array[i] == array[j]) print(array[i]);
     }
+  }
+}
+
+void reverseList() {
+  final array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final reversedList = <int>[];
+  for (var i = array.length - 1; i >= 0; i--) {
+    reversedList.add(array[i]);
+  }
+}
+
+void quickSort() {
+  final array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final reversedList = <int>[];
+  for (var i = array.length - 1; i >= 0; i--) {
+    reversedList.add(array[i]);
   }
 }
 
---
+++ complexity.md
@@ -0,0 +1,19 @@
+# Algorithms Complexity
+
+Simple `for` complexity: `2n+2`
+
+```dart
+        1   n+1     n
+    for(int i=0;i<n;i++){
+
+    }
+```
+
+`for` with two increments: `n+2`
+
+```dart
+        1    (n/2+1)  n/2
+    for(int i=0;i<n;i+=2){
+
+    }
+```
